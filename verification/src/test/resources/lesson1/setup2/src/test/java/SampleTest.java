416d60ac165180e88f6e9d66d92efed4e31d3cfc
Tomasz Fijalkowski <tomasz.fijalkowski@allegro.pl>
2020-09-14 20:19:24 +0200

    lesson1 junit

---
+++ verification/src/test/resources/lesson1/setup2/src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+package lesson1.src.test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SampleTest {
+
+    @Test
+    void test1() {
+        assertTrue(true, "Sample assertion");
+    }
+}
